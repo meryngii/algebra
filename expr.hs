6cc1a269ce49692e864b84b7127c394e9861805f
meryngii <meryngii+git@gmail.com>
2012-09-19 03:55:29 +0900

    simplified codes

--- expr.hs
+++ expr.hs
@@ -87,14 +87,6 @@ lexer (x:xs)
 parse = fst . expression . lexer
 
 
-
-isConst :: Expr -> Bool
-isConst (Const _) = True
-isConst _ = False
-
-getConst :: Expr -> Integer
-getConst (Const x) = x
-
 assign (Symbol p) p' = a
     where
         a :: Expr -> Expr
@@ -103,6 +95,38 @@ assign (Symbol p) p' = a
         a (Prod x) = Prod (map a x)
         a (Power x y) = Power (a x) (a y)
         a x = x
+
+isConst :: Expr -> Bool
+isConst (Const _) = True
+isConst _ = False
+
+getConsts :: [Expr] -> [Integer]
+getConsts e = [g v | v<-e, isConst v]
+    where g (Const x) = x
+
+getTerms :: Expr -> Expr
+getTerms (Prod x) = simplify $ Prod [v | v<-x, not $ isConst v]
+getTerms (Symbol x) = Symbol x
+getTerms (Const _) = Const 1
+getTerms (Power x y) = Power x y
+
+getConstTerm :: Expr -> Integer
+getConstTerm (Prod x) = product $ getConsts x
+getConstTerm (Symbol _) = 1
+getConstTerm (Const x) = x
+getConstTerm (Power _ _) = 1
+
+getBase :: Expr -> Expr
+getBase (Const x) = Const x
+getBase (Symbol x) = Symbol x
+getBase (Power x y) = x
+getBase (Sum x) = Sum x
+
+getExpo :: Expr -> Expr
+getExpo (Const x) = Const 1
+getExpo (Symbol x) = Const 1
+getExpo (Power x y) = y
+getExpo (Sum x) = Const 1
 
 
 simplify :: Expr -> Expr
@@ -115,28 +139,16 @@ simplify (Sum x)
         where
             add' :: [Expr] -> Expr -> [Expr]
             add' x (Sum y) = foldl add' x y
-
             add' x (Const y)
                 = [v | v<-x, not $ isConst v] ++
-                    case sum $ y : [getConst v | v<-x, isConst v] of
+                    case sum $ y : getConsts x of
                         0 -> []
                         x -> [Const x]
-
             add' x y
                 = [v | v<-x, not $ getTerms v `equals` getTerms y] ++
                     [simplify $
                         (Const . sum $ [getConstTerm v | v<-x, getTerms v `equals` getTerms y] ++ [getConstTerm y])
                             `mul` getTerms y]
-
-            getTerms (Prod x) = simplify . Prod . sort $ [v | v<-x, not . isConst $ v]
-            getTerms (Symbol x) = Symbol x
-            getTerms (Const _) = Const 1
-            getTerms (Power x y) = Power x y
-
-            getConstTerm (Prod x) = maybe 1 getConst . find isConst $ x
-            getConstTerm (Symbol _) = 1
-            getConstTerm (Const x) = x
-            getConstTerm (Power _ _) = 1
 
 simplify (Prod x)
     = case foldl mul' [] . map simplify $ x of
@@ -146,29 +158,17 @@ simplify (Prod x)
         where
             mul' :: [Expr] -> Expr -> [Expr]
             mul' x (Prod y) = foldl mul' x y
-
             mul' x (Const y)
-                = case product $ y : [getConst v | v<-x, isConst v] of                
+                = case product $ y : getConsts x of                
                     0 -> [Const 0]
                     1 -> r
                     x -> Const x : r
                     where r = [v | v<-x, not $ isConst v]
 
-            mul' x y
                 = [v | v<-x, not $ getBase v `equals` getBase y] ++
                     [simplify $
                         Power (getBase y)
                             (foldl1 add $ [getExpo v | v<-x, getBase v `equals` getBase y] ++ [getExpo y])]
-
-            getBase (Const x) = Const x
-            getBase (Symbol x) = Symbol x
-            getBase (Power x y) = x
-            getBase (Sum x) = Sum x
-
-            getExpo (Const x) = Const 1
-            getExpo (Symbol x) = Const 1
-            getExpo (Power x y) = y
-            getExpo (Sum x) = Const 1
 
 simplify (Power x y)
     = pow' (simplify x) (simplify y)
@@ -181,8 +181,9 @@ simplify (Power x y)
             pow' (Const x) (Const y) = Const (x^y)
             pow' x y = Power x y
 
+--pow' (Prod x) (Const y) = simplify $ Prod $ (Const $ (^y) $ product $ getConsts x) : Power (Prod [v | v<-x, not $ isConst v]) (Const y)  -- endless
+
 simplify (Const x) = Const x
 simplify (Symbol x) = Symbol x
-
 
 
